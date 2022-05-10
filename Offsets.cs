c109ebbbbb4ec6eed12397b04dd4046b3fd371b8
Poptart <70210614+PoptartWeScript@users.noreply.github.com>
2022-05-10 10:10:46 -0500

    Update Offsets.cs

--- Offsets.cs
+++ Offsets.cs
@@ -5,8 +5,8 @@ namespace DeadByDaylight
 public class Offsets
 
 {
-   public static Int64 UWorld = 0xB846420;
-   public static Int64 GNames = 0xB667040;
+   public static Int64 UWorld = 0xB6FB080;
+   public static Int64 GNames = 0xB51C880;
 
 public class UE
 {
