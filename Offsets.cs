9aff0385174346801592294d6ae65ff2f7a490cb
Poptart <70210614+PoptartWeScript@users.noreply.github.com>
2022-07-19 10:30:33 -0500

    Update Offsets.cs

--- Offsets.cs
+++ Offsets.cs
@@ -5,8 +5,8 @@ namespace DeadByDaylight
 public class Offsets
 
 {
-   public static Int64 UWorld = 0xB9882E0;
-   public static Int64 GNames = 0xB7A9840;
+   public static Int64 UWorld = 0xbe0a4e0;
+   public static Int64 GNames = 0xbc2b8c0;
 
 public class UE
 {
@@ -35,32 +35,32 @@ public class UE
 
    public class APlayerController
 {
-       public static Int64 APawn = 0x2B8;
-       public static Int64 PlayerCameraManager = 0x2D0;
+       public static Int64 APawn = 0x2b8;
+       public static Int64 PlayerCameraManager = 0x2d0;
 }
 
    public class APawn
 {
-       public static Int64 UPlayerInteractionHandler = 0x8F8;
+       public static Int64 UPlayerInteractionHandler = 0x8f8;
 }
 
    public class UPlayerInteractionHandler
 {
-       public static Int64 _skillCheck = 0x2E8;
+       public static Int64 _skillCheck = 0x2e8;
 }
 
    public class AActor
 {
        public static Int64 ULocalRoot = 0x140;
        public static Int64 USceneComponent = 0x140;
-       public static Int64 tempVec= 0x118; //but weirdly is  = 0x134;
+       public static Int64 tempVec= 0x118; //but weirdly is  = 0x0;
        public static Int64 HealthPointer = 0x13F8;
        public static Int64 IsActorDead = 0x1E4;
        public static Int64 Healthy = 0x1E0;
        public static Int64 HasBeenSearched = 0x364;
        public static Int64 IsCleansed = 0x378;
        public static Int64 isRepaired = 0x339;
-       public static Int64 HasBeenSet = 0x4B0;
+       public static Int64 HasBeenSet = 0x4b0;
        public static Int64 currentProgressPercent = 0x348;
 }
 
